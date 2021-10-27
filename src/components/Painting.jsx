a1110b87c94da8808469592f0cf8873cc1175bd2
Eduard-Konovka <ed098ua@gmail.com>
2021-10-27 22:00:18 +0300

    start

---
+++ src/components/Painting.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import defaultImage from '../images/default.jpg';
+
+export default function Painting({
+  imageUrl = defaultImage,
+  title,
+  authorUrl,
+  authorTag = 'неизвестен',
+  price,
+  quantity = 0,
+}) {
+  return (
+    <div>
+      <img src={imageUrl ?? defaultImage} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={authorUrl}>{authorTag}</a>
+      </p>
+      <p>Цена: {price} кредитов</p>
+      <p>Доступность: {quantity > 9 ? 'есть в наличии' : 'заканчивается'}</p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+}
+
+Painting.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  authorUrl: PropTypes.string.isRequired,
+  authorTag: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+};
